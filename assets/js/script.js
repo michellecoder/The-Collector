702f8884ed9c1d1bbf65d73ebe14102970fa3c7e
j92pruitt <j92pruitt@gmail.com>
2021-06-22 20:09:24 -0700

    Finished autocomplete refactor and cleaning

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,24 +39,30 @@ function handleSubmit(event){
 // Autocomplete work starts here:
 
 function autocomplete(inputElement) {
-    /*the autocomplete function takes two arguments,
-    the text field element and an array of possible autocompleted values:*/
+    // Autocomplete funciton takes in one value. An input element wrapped in jquery.
+    // currentFocus is a global variable used to help with making the autocomplete keyboard accessible.
     var currentFocus;
-    /*execute a function when someone writes in the text field:*/
-    inputElement.on('input', function (e) {
-        var a, val = this.value;
-        /*close any already open lists of autocompleted values*/
-        closeAllLists();
-        if (!val) { return false; }
-        currentFocus = -1;
-        /*create a DIV element that will contain the items (values):*/
-        a = document.createElement("DIV");
-        a.setAttribute("id", this.id + "-autocomplete-list");
-        a.setAttribute("class", "autocomplete-items");
-        /*append the DIV element as a child of the autocomplete container:*/
-        this.parentNode.appendChild(a);
 
-        var autocompleteRequestUrl = `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${val}&apikey=${marvelApiKey}`
+    // Function will execute on any input in the supplied element.
+    inputElement.on('input', function (event) {
+        // Sets two scoped variables for later use then makes sure that any previous autocomplete list is closed.
+        var autocompleteList;
+        var currentInputValue = this.value;
+        closeAllLists();
+
+        // Checks if there is a value in the input field. If not fuction halts.
+        if (!currentInputValue) {
+            return false
+        };
+
+        currentFocus = -1;
+
+        autocompleteList = $(`<div class="autocomplete-items" id="${this.id}-autocomplete-list" ></div>`);
+        $(this).parent().append(autocompleteList);
+
+        var autocompleteRequestUrl = `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${currentInputValue}&apikey=${marvelApiKey}`
+
+        // Request to Marvel API in order to get valid names starting with currentInputValue
         fetch(autocompleteRequestUrl)
         .then(function(response){
             // Basic error handling, needs refinement based on return code.
@@ -68,38 +74,38 @@ function autocomplete(inputElement) {
             return response.json()
         })
         .then(function(data){
-            var arr =getValidNames(data)
-            createAutocompleteItems(a, val, arr)
+            var arr = getValidNames(data)
+            createAutocompleteItems(autocompleteList, currentInputValue, arr)
         })
     });
 
-    function createAutocompleteItems(a, val, arr){
-        var b;
-        /*for each item in the array...*/
+    function createAutocompleteItems(autocompleteList, currentInputValue, arr){
+        var autocompleteItem;
+
         for (var i = 0; i < arr.length; i++) {
-            /*check if the item starts with the same letters as the text field value:*/
-            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-                /*create a DIV element for each matching element:*/
-                b = document.createElement("DIV");
-                /*make the matching letters bold:*/
-                b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-                b.innerHTML += arr[i].substr(val.length);
-                /*insert a input field that will hold the current array item's value:*/
-                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-                /*execute a function when someone clicks on the item value (DIV element):*/
-                b.addEventListener("click", function (e) {
-                    /*insert the value for the autocomplete text field:*/
-                    inputElement.value = this.getElementsByTagName("input")[0].value;
-                    /*close the list of autocompleted values,
-                    (or any other open lists of autocompleted values:*/
+            // Extra check in case the api returns entries which do not begin with the correct substring
+            if (arr[i].substr(0, currentInputValue.length).toUpperCase() == currentInputValue.toUpperCase()) {
+                autocompleteItem = $(
+                                    `
+                                    <div>
+                                        <strong>${arr[i].substr(0, currentInputValue.length)}</strong>${arr[i].substr(currentInputValue.length)}
+                                        <input type="hidden" value="${arr[i]}"
+                                    </div>
+                                    `);
+                
+                // Click event populates input with clicked text value
+                autocompleteItem.click(function (e) {
+                    inputElement.val(this.getElementsByTagName("input")[0].value);
                     closeAllLists();
                 });
-                a.appendChild(b);
+
+                autocompleteList.append(autocompleteItem);
             }
         }
     }
 
     function getValidNames(apiData) {
+        // Takes a Marvel API repsonse's json data and extracts only the names to an array.
         var resultsArray = apiData.data.results;
         var namesArray = [];
         for (var i = 0; i < resultsArray.length; i++){
@@ -109,60 +115,67 @@ function autocomplete(inputElement) {
         return namesArray;
     }
 
-    /*execute a function presses a key on the keyboard:*/
-    inputElement.keydown(function (e) {
-        var x = document.getElementById(this.id + "-autocomplete-list");
-        if (x) x = x.getElementsByTagName("div");
-        if (e.keyCode == 40) {
-            /*If the arrow DOWN key is pressed,
-            increase the currentFocus variable:*/
+    inputElement.keydown(function (event) {
+        var autocompleteList = document.getElementById(`${this.id}-autocomplete-list`);
+
+        if (autocompleteList) {
+            autocompleteList = autocompleteList.getElementsByTagName("div");
+        }
+
+        if (event.keyCode == 40) {
+            // Triggers if down key is pressed; moves the focused area down the list.
             currentFocus++;
-            /*and and make the current item more visible:*/
-            addActive(x);
-        } else if (e.keyCode == 38) { //up
-            /*If the arrow UP key is pressed,
-            decrease the currentFocus variable:*/
+            addActive(autocompleteList);
+        } else if (event.keyCode == 38) { //up
+            // Triggers if up key is pressed; moves the focused area up the list.
             currentFocus--;
-            /*and and make the current item more visible:*/
-            addActive(x);
-        } else if (e.keyCode == 13) {
-            /*If the ENTER key is pressed, prevent the form from being submitted,*/
-            e.preventDefault();
+            addActive(autocompleteList);
+        } else if (event.keyCode == 13) {
+            // Triggers if enter key is pressed; moves the focused area down the list.
+            event.preventDefault();
             if (currentFocus > -1) {
-                /*and simulate a click on the "active" item:*/
-                if (x) x[currentFocus].click();
+                if (autocompleteList) {
+                    autocompleteList[currentFocus].click();}
             }
         }
     });
-    function addActive(x) {
-        /*a function to classify an item as "active":*/
-        if (!x) return false;
-        /*start by removing the "active" class on all items:*/
-        removeActive(x);
-        if (currentFocus >= x.length) currentFocus = 0;
-        if (currentFocus < 0) currentFocus = (x.length - 1);
-        /*add class "autocomplete-active":*/
-        x[currentFocus].classList.add("autocomplete-active");
+
+    function addActive(autocompleteItems) {
+        // Helper function to add active class to autocomplete items
+        if (!autocompleteItems) {
+            return false
+        };
+
+        removeActive(autocompleteItems);
+
+        // Handles behavior at beginning and end of list
+        if (currentFocus >= autocompleteItems.length) {
+            currentFocus = 0
+        };
+        if (currentFocus < 0) {
+            currentFocus = (autocompleteItems.length - 1)
+        };
+
+        autocompleteItems[currentFocus].classList.add("autocomplete-active");
     }
-    function removeActive(x) {
-        /*a function to remove the "active" class from all autocomplete items:*/
-        for (var i = 0; i < x.length; i++) {
-            x[i].classList.remove("autocomplete-active");
+
+    function removeActive(autocompleteItems) {
+        for (var i = 0; i < autocompleteItems.length; i++) {
+            autocompleteItems[i].classList.remove("autocomplete-active");
         }
     }
-    function closeAllLists(elmnt) {
-        /*close all autocomplete lists in the document,
-        except the one passed as an argument:*/
-        var x = document.getElementsByClassName("autocomplete-items");
-        for (var i = 0; i < x.length; i++) {
-            if (elmnt != x[i] && elmnt != inputElement) {
-                x[i].parentNode.removeChild(x[i]);
+
+    function closeAllLists(htmlElement) {
+        var autocompleteItems = $(".autocomplete-items");
+        for (var i = 0; i < autocompleteItems.length; i++) {
+            if (htmlElement != autocompleteItems[i] && htmlElement != inputElement) {
+                autocompleteItems[i].remove();
             }
         }
     }
-    /*execute a function when someone clicks in the document:*/
-    document.addEventListener("click", function (e) {
-        closeAllLists(e.target);
+
+    document.addEventListener("click", function (event) {
+        closeAllLists(event.target);
     });
 }
 
